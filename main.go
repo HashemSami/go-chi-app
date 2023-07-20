31fad4aaa15581ad66ef261c9729382282b718ab
hashemsami <hashem.sf@gmail.com>
2023-07-20 05:17:12 +0300

    using-chi

--- main.go
+++ main.go
@@ -45,8 +45,11 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+	r.Get("/", homeHendler)
+	r.Get("/contact", contactHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not Found", http.StatusNotFound)
 	})
+	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
