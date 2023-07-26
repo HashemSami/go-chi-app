421c7dba9738e64a7099ed0eb4ee999a20d0e602
hashemsami <hashem.sf@gmail.com>
2023-07-26 10:55:00 +0300

    adding users singup controller

---
+++ controllers/users.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/HashemSami/go-chi-app/views"
+)
+
+type Users struct {
+	Templates struct {
+		New views.Template
+	}
+}
+
+func (u Users) New(w http.ResponseWriter, r *http.Request) {
+	// we need a view to render
+	u.Templates.New.Execute(w, nil)
+}
--- main.go
+++ main.go
@@ -32,12 +32,16 @@ func main() {
 		views.ParseFS(templates.FS, "signup.html", "tailwind.html"),
 	)
 
+	// creating users controllers
+	usersC := controllers.Users{}
+	usersC.Templates.New = signupTpl
+
 	r.Use(middleware.Logger)
 
 	r.Get("/", controllers.StaticHandler(homeTpl))
 	r.Get("/contact", controllers.StaticHandler(contactTpl))
 	r.Get("/faq", controllers.FAQ(faqTpl))
-	r.Get("/signup", controllers.StaticHandler(signupTpl))
+	r.Get("/signup", usersC.New)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not Found", http.StatusNotFound)
