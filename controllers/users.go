d974af7d6eb351c875beaaeb6c5f063b60b0de71
hashemsami <hashem.sf@gmail.com>
2023-07-26 12:56:05 +0300

    woring on parsin ghte form

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -13,4 +14,19 @@ type Users struct {
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	// we need a view to render
 	u.Templates.New.Execute(w, nil)
+}
+
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	// before calling post form, we must call
+	// the parse form function first to be able to use the
+	// post request as a form
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// getting the same name attribute used in the html
+	fmt.Fprint(w, "Email: ", r.PostForm.Get("email"))
+	fmt.Fprint(w, "Password: ", r.PostForm.Get("password"))
 }
--- main.go
+++ main.go
@@ -42,6 +42,7 @@ func main() {
 	r.Get("/contact", controllers.StaticHandler(contactTpl))
 	r.Get("/faq", controllers.FAQ(faqTpl))
 	r.Get("/signup", usersC.New)
+	r.Post("/users", usersC.Create)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not Found", http.StatusNotFound)
