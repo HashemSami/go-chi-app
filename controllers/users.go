4210deb9f56691707e310c0ccfd4185f662383a3
hashemsami <hashem.sf@gmail.com>
2023-07-27 10:45:01 +0300

    parsing the users form

--- controllers/users.go
+++ controllers/users.go
@@ -16,17 +16,24 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	u.Templates.New.Execute(w, nil)
 }
 
-func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	// before calling post form, we must call
-	// the parse form function first to be able to use the
-	// post request as a form
-	err := r.ParseForm()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+// func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+// 	// before calling post form, we must call
+// 	// the parse form function first to be able to use the
+// 	// post request as a form
+// 	err := r.ParseForm()
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusBadRequest)
+// 		return
+// 	}
 
+// 	// getting the same name attribute used in the html
+// 	fmt.Fprint(w, "Email: ", r.PostForm.Get("email"))
+// 	fmt.Fprint(w, "Password: ", r.PostForm.Get("password"))
+// }
+
+// another version
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 	// getting the same name attribute used in the html
-	fmt.Fprint(w, "Email: ", r.PostForm.Get("email"))
-	fmt.Fprint(w, "Password: ", r.PostForm.Get("password"))
+	fmt.Fprint(w, "Email: ", r.FormValue("email"))
+	fmt.Fprint(w, "Password: ", r.FormValue("password"))
 }
